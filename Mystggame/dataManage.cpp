d1764377a79cc949dcb2b9f3694bae48b56d9ba9
一鸣 胡 <dandanegg@outlook.com>
2025-06-09 10:18:49 +0800

    first commit

---
+++ Mystggame/dataManage.cpp
@@ -0,0 +1,53 @@
+#include<vector>
+#include<stack>
+template<typename T>
+class node
+{
+public:
+	int lastPos;
+	int nextPos;
+	T value;
+	bool exist;
+};
+template <typename T>
+class chain {
+protected:
+	vector<node<T> > datas(1);
+	stack<int> freed;
+public:
+	chain() {
+		datas[0].nextPos = 0;
+		datas[0].lastPos = 0;
+		datas[0].exist = 1;
+	}
+	void insert(const & T a) {
+		if(freed.empty()){
+			datas.push_back({ datas[0].lastPos,0,a,1 });
+			datas[datas[0.lastPos]].nextPos = datas.size() - 1;
+			datas[0].lastPos = datas.size() - 1;
+		}
+		else {
+			datas[datas[0.lastPos]].nextPos = fread.top();
+			datas[0].lastPos = datas.size() - 1;
+			datas[freed.top()].value = a;
+			datas[]
+		}
+	}
+	void remove (int pos) { //ÒÆ³ý½Úµã
+		datas[datas[pos].lastPos].nextPos = datas[pos].nextPos;
+		datas[datas[pos].nextPos].lastPos = datas[pos].lastPos;
+		datas[pos].exist = false;
+	}
+	int next(int &pos) {
+		pos = datas[pos].nextPos;
+		return pos;
+	}
+	int back(int& pos) {
+		pos = datas[pos].lastPos;
+		return pos;
+	}
+	T& operator[](int pos) {
+		return datas[pos];
+	}
+
+};
